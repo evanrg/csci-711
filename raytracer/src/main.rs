e27272f65f6d6687461c3c3104ada50c3dcea547
erg4838 <erg4838@rit.edu>
2026-01-28 09:35:23 -0500

    Scene complete.

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -14,11 +14,14 @@ mod world;
 
 fn main() {
     // Get our objects setup
-    let sphere_color = Vec3::new(1.0, 0.0, 0.0);
-    let sphere_material = Material::new(sphere_color);
+    let sphere_1_color = Vec3::new(0.5, 0.5, 0.5);
+    let sphere_1_material = Material::new(sphere_1_color);
 
-    let triangle_1_color = Vec3::new(0.0, 0.0, 1.0);
-    let triangle_2_color = Vec3::new(0.0, 1.0, 0.0);
+    let sphere_2_color = Vec3::new(1.0, 1.0, 1.0);
+    let sphere_2_material = Material::new(sphere_2_color);
+
+    let triangle_1_color = Vec3::new(1.0, 0.0, 0.0);
+    let triangle_2_color = Vec3::new(1.0, 1.0, 0.0);
 
     let triangle_1_material = Material::new(triangle_1_color);
     let triangle_2_material = Material::new(triangle_2_color);
@@ -36,22 +39,25 @@ fn main() {
     let t2_v2 = Vec3::new(1.0, 1.0, 0.0);
     let t2_v3 = Vec3::new(0.0, 1.0, 0.0);
 
-    let mut triangle_blue = Triangle::new((t1_v1, t1_v2, t1_v3), triangle_1_material);
-    let mut triangle_green = Triangle::new((t2_v1, t2_v2, t2_v3), triangle_2_material);
+    let mut triangle_1 = Triangle::new((t1_v1, t1_v2, t1_v3), triangle_1_material);
+    let mut triangle_2 = Triangle::new((t2_v1, t2_v2, t2_v3), triangle_2_material);
 
-    triangle_blue.translate_mut(Vec3::new(-6.0, -6.0, 0.0));
-    triangle_blue.scale_mut(Vec3::new(20.0, 100.0, 1.0));
-    triangle_blue.rotate_x_mut(-90.0);
+    triangle_1.translate_mut(Vec3::new(-8.0, -6.0, -10.0));
+    triangle_1.scale_mut(Vec3::new(30.0, 200.0, 1.0));
+    triangle_1.rotate_x_mut(-90.0);
 
-    triangle_green.translate_mut(Vec3::new(-6.0, -6.0, 0.0));
-    triangle_green.scale_mut(Vec3::new(20.0,100.0, 1.0));
-    triangle_green.rotate_x_mut(-90.0);
+    triangle_2.translate_mut(Vec3::new(-8.0, -6.0, -10.0));
+    triangle_2.scale_mut(Vec3::new(30.0, 200.0, 1.0));
+    triangle_2.rotate_x_mut(-90.0);
 
-    triangle_blue.compile_model();
-    triangle_green.compile_model();
+    triangle_1.compile_model();
+    triangle_2.compile_model();
 
-    let mut sphere = Sphere::new(sphere_pos, sphere_radius, sphere_material);
-    sphere.translate_mut(Vec3::new(0.0, 0.0, -10.0));
+    let mut sphere_gray = Sphere::new(sphere_pos, sphere_radius, sphere_1_material);
+    sphere_gray.translate_mut(Vec3::new(-1.0, 0.5, -1.5));
+
+    let mut sphere_white = Sphere::new(sphere_pos, sphere_radius, sphere_2_material);
+    sphere_white.translate_mut(Vec3::new(1.5, -1.0, -6.0));
 
     // Setup camera
     let camera_pos = Vec3::new(0.0, 0.0, 10.0);
@@ -60,7 +66,7 @@ fn main() {
 
     let focal_length = 5.0;
     let img_dim = (800, 600);
-    let film_plane_dim = (5, 5);
+    let film_plane_dim = (4, 3);
 
     let camera = Camera::new(
         camera_pos,
@@ -72,13 +78,13 @@ fn main() {
     );
 
     // Create our world
-    let bg_color = Vec3::new(1.0, 1.0, 1.0);
+    let bg_color = Vec3::new(0.5, 0.9, 1.0);
     let mut world = World::new(bg_color);
 
-    world.add(sphere);
-    world.add(triangle_blue);
-    world.add(triangle_green);
-    
+    world.add(sphere_gray);
+    world.add(sphere_white);
+    world.add(triangle_1);
+    world.add(triangle_2);
 
     // convert to world space
     world.objects_to_world_space();
